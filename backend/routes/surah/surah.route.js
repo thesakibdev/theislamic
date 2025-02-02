f6fc25c64d6168d49a0d912f6830b412d1edacd9
The Sakib Dev <139985754+thesakibdev@users.noreply.github.com>
2025-02-02 23:29:02 +0600

    router setup

--- backend/routes/surah/surah.route.js
+++ backend/routes/surah/surah.route.js
@@ -1,20 +1,38 @@
 const express = require("express");
 const router = express.Router();
 const {
+  // main verses
   addVerse,
+  editArabicAyah,
+  deleteArabicAyah,
+
+  // verseOtherData
   addVerseOtherData,
+  editVerseOtherData,
+  deleteVerseOtherData,
+
+  // get main surahs
   getAllSurahs,
   getAllSurahsPaginated,
-  deleteVerse,
-  editVerse,
 } = require("../../controllers/surah/surah.controller");
 
 router.post("/surah/add/verse", addVerse);
+router.put(
+  "/surahs/:surahNumber/verses/:verseNumber/edit-arabic",
+  editArabicAyah
+);
+router.delete("/surahs/:surahNumber/verses/:verseNumber", deleteArabicAyah);
+
+// verse data
 router.post("/surah/add/verse-other", addVerseOtherData);
-
-router.put("/surah/:surahNumber/verse/:verseNumber", editVerse);
-
-router.delete("/surah/:surahNumber/verse/:verseNumber", deleteVerse);
+router.put(
+  "/surah/verse-other-data/:surahNumber/:verseNumber/:language",
+  editVerseOtherData
+);
+router.delete(
+  "/surah/verse-other-data/:surahNumber/:verseNumber/:language",
+  deleteVerseOtherData
+);
 
 router.get("/surah/all", getAllSurahs);
 
