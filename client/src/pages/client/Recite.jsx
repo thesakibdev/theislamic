f85e430d78ad94f0c3868ab98bf8445fc9ecd853
masaud04 <ahmedmasaud942@gmail.com>
2025-02-13 21:56:42 +0600

    fetch data in reading mode and transliteration page

--- client/src/pages/client/Recite.jsx
+++ client/src/pages/client/Recite.jsx
@@ -1,14 +1,5 @@
 import { Button } from "@/components/ui/button";
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
   useGetAllSurahsNameQuery,
@@ -19,24 +10,47 @@ import { useParams } from "react-router-dom";
 import { LuNotebookTabs } from "react-icons/lu";
 
 export default function RecitePage() {
-  // const { number } = useParams();
-  // // surah data
-  // const { data: allSurah } = useGetAllSurahsQuery();
-  // const surah = allSurah.surahs.find(
-  //   (surah) => surah.surahNumber === Number(number)
-  // );
+  const { number } = useParams();
   // // surah list
-  // const { data: allSurahName } = useGetAllSurahsNameQuery();
-  // const allSurahs = allSurahName.map((surah) => surah.surahName);
+  const { data: allSurahName } = useGetAllSurahsNameQuery();
+  const allSurahs = allSurahName.map((surah) => surah.surahName);
+  console.log(allSurahs);
 
-  // console.log(allSurahs);
+  //  data
+  const { data: allSurah } = useGetAllSurahsQuery();
+  const transliteration = allSurah?.surahs?.find(
+    (surah) => surah.surahNumber === Number(number)
+  );
+  const allAyah = transliteration?.verses?.map((verse) => ({
+    verseNumber: verse.verseOtherData,
+    arabicAyah: verse.arabicAyah,
+  }));
+  // console.log(allAyah?.map((ayah) => ayah.arabicAyah));
+  const currentSurah = allAyah?.map((ayah) => ayah.arabicAyah);
+  const translateAyah = allAyah?.map((ayah) => ayah.verseNumber);
+  const tranlateEn = translateAyah?.map(
+    (ayah) => ayah.find((ayah) => ayah.language === "en").translation
+  );
+  const tranlateEng = allAyah?.map((ayah) => {
+    return (
+      ayah.translations?.find((t) => t.language === "en")?.translation ||
+      "Translation not available"
+    );
+  });
+  console.log(tranlateEng);
+  console.log(currentSurah);
+  console.log(tranlateEn);
+  // console.log(translateAyah?.map((ayah) => ayah.find((ayah) => ayah.language === 'en').translation));
+  // console.log(transliteration.verses[0].arabicAyah);
+  // console.log(transliteration.verses[0].verseOtherData[1].translation);
+
   return (
     <>
       <section className="pt-10">
         <h2>Recite page</h2>
         <div className="container mx-auto px-4">
           <div className="flex flex-col md:flex-row">
-            <aside className="w-0 md:w-1/4 max-h-screen h-full">
+            <aside className="w-0 md:w-2/6 max-h-screen h-full">
               {/* sidebar for desktop version */}
               <div className="hidden border-r-2 md:block p-5">
                 <h3 className="text-2xl font-semibold underline cursor-pointer">
@@ -71,12 +85,20 @@ export default function RecitePage() {
                       </div>
                       <div className="mt-5 bg-[#80BDA9] max-h-[800px] overflow-auto rounded-md">
                         <ul>
-                          {Array.from({ length: 24 }).map((_, index) => (
+                          {/* {Array.from({ length: 24 }).map((_, index) => (
+                            <li
+                            key={index}
+                            className="hover:bg-[#80BDA9] hover:text-white/90 border-b-2 text-white text-[27px]  text-center cursor-pointer p-3"
+                            >
+                            {index + 1}
+                            </li>
+                            ))} */}
+                          {allSurahs?.map((surah, index) => (
                             <li
                               key={index}
-                              className="hover:bg-[#80BDA9] hover:text-white/90 border-b-2 text-white text-[27px]  text-center cursor-pointer p-3"
+                              className="hover:bg-[#80BDA9] hover:text-white/90 border-b-2 text-white text-lg md:text-xl  text-center cursor-pointer p-3"
                             >
-                              {index + 1}
+                              {surah}
                             </li>
                           ))}
                         </ul>
@@ -147,9 +169,9 @@ export default function RecitePage() {
                 </Sheet>
               </div>
             </aside>
-            <div className="p-5 flex justify-center  w-full">
+            <div className="p-5 flex justify-center w-full">
               <Tabs defaultValue="reading" className="my-5">
-                <TabsList className="grid w-full grid-cols-2 justify-start">
+                <TabsList className="grid w-full grid-cols-1 md:grid-cols-2 justify-start">
                   <TabsTrigger
                     className="p-0 data-[state=active]:rounded-full"
                     value="transliteration"
@@ -163,8 +185,8 @@ export default function RecitePage() {
                     Reading
                   </TabsTrigger>
                 </TabsList>
-                <TabsContent value="transliteration" className="max-w-3xl">
-                  <div className="flex flex-col justify-center min-w-full md:min-w-2xl">
+                <TabsContent value="transliteration">
+                  <div className="flex flex-col justify-center max-w-3xl min-w-full md:min-w-2xl">
                     <h2 className="text-2xl md:text-3xl text-center font-bold">
                       بِسْمِ اللّٰهِ الرَّحْمٰنِ الرَّحِیْمِ
                     </h2>
@@ -178,40 +200,57 @@ export default function RecitePage() {
                           Dr. Mustafa Khattab, The Clear Quran Translation
                         </p>
                       </div>
-                      {Array.from({ length: 10 }).map((_, index) => (
-                        <div
-                          key={index}
-                          className={`flex flex-col items-center gap-1 my-2`}
-                        >
-                          <p className="w-5/6 text-2xl text-right mt-2 text-black">
+                      {/* {Array.from({ length: 10 }).map((_, index) => ( */}
+                      <div
+                        // key={index}
+                        className={`flex flex-col items-center gap-1 my-2`}
+                      >
+                        {/* <p className="w-5/6 text-2xl text-right mt-2 text-black">
                             بِسْمِ ٱللَّهِ ٱلرَّحْمَـٰنِ ٱلرَّحِيمِ ١
                             ٱلْحَمْدُٱللَّهِ ٱلرَّحْمَـٰنِ ٱلرَّحِيمِ ١
                             ٱلْحَمْدُ
                           </p>
-                          <p className="w-5/6 text-2xl text-left mt-2 text-black">
-                            {" "}
+                            <p className="w-5/6 text-2xl text-left mt-2 text-black">
+                              {" "}
+                              this is english translation{" "}
+                            </p> */}
+                        {currentSurah?.map((ayah, index) => (
+                          <p
+                            key={index}
+                            className="w-5/6 text-2xl text-right mt-2 text-black"
+                          >
+                            {ayah}
+                          </p>
+                        ))}
+                        {tranlateEn?.map((en, index) => (
+                          <p
+                            key={index}
+                            className="w-5/6 text-2xl text-left mt-2 text-black"
+                          >
+                            {en}
                             this is english translation{" "}
                           </p>
-                        </div>
-                      ))}
+                        ))}
+                      </div>
+                      {/* ))} */}
                     </div>
                   </div>
                 </TabsContent>
                 <TabsContent value="reading">
-                  <div className="flex flex-col text-center justify-center w-full">
+                  <div className="flex flex-col text-center justify-center max-w-3xl w-full">
                     <h2 className="text-2xl md:text-3xl font-bold">
                       بِسْمِ اللّٰهِ الرَّحْمٰنِ الرَّحِیْمِ
                     </h2>
                     <div className="mt-4 flex flex-col">
-                      {/* {surah.verses.map((verse, index) => (
+                      {currentSurah?.map((ayah, index) => (
                         <p
                           key={index}
                           className="text-2xl mt-2 text-black text-center"
                         >
-                          {verse.arabicAyah}
+                          {ayah}
                         </p>
-                      ))} */}
-                      {Array.from({ length: 10 }).map((_, index) => (
+                      ))}
+                      {/* {Array.from({ length: 10 }).map((_, index) => (
                         <p
                           key={index}
                           className="text-2xl mt-2 text-black text-center"
@@ -219,7 +258,7 @@ export default function RecitePage() {
                           بِسْمِ ٱللَّهِ ٱلرَّحْمَـٰنِ ٱلرَّحِيمِ ١ ٱلْحَمْدُ
                           لِلَّهِ رَبِّ ٱلْعَـٰلَمِينَ ٢
                         </p>
-                      ))}
+                      ))} */}
                     </div>
                   </div>
                 </TabsContent>
