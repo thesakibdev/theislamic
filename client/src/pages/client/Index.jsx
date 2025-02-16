88f5629c86687a9fbc43aa87ef78ebdd30a871cf
The Sakib Dev <139985754+thesakibdev@users.noreply.github.com>
2025-02-16 19:23:29 +0600

    index page done

--- client/src/pages/client/Index.jsx
+++ client/src/pages/client/Index.jsx
@@ -9,6 +9,7 @@ import {
   TabsTrigger,
 } from "../../components/ui/tabs";
 import { useNavigate } from "react-router-dom";
+import RubAlHizb from "@/assets/icon/RubAlHizb";
 
 export default function IndexPage() {
   const navigate = useNavigate();
@@ -131,9 +132,12 @@ export default function IndexPage() {
 
   const { data: surahs, isLoading } = useGetAllSurahsNameQuery();
 
+  let recentSurahs = JSON.parse(localStorage.getItem("surahs")) || [];
+  console.log(recentSurahs);
+
   return (
     <main>
-      <section className="pt-[4.5rem] md:pt-40 pb-[50px]">
+      <section className="pt-[4.5rem] md:pt-30 pb-[5px]">
         <div className="container mx-auto px-4">
           <div className="flex flex-col md:flex-row items-center gap-5 md:gap-10">
             <img src={IndexPageBanner} alt="banner" />
@@ -178,7 +182,21 @@ export default function IndexPage() {
                 Bookmarks
               </TabsTrigger>
             </TabsList>
-            <TabsContent value="recent">No Recent Read</TabsContent>
+            <TabsContent value="recent">
+              {recentSurahs.length > 0 ? (
+                <div className="flex  gap-2">
+                  {recentSurahs.map((surah) => (
+                    <p
+                      key={surah.surahNumber}
+                      className="text-md font-bold cursor-pointer hover:text-primary-foreground"
+                      onClick={() => navigate(`/recite/${surah.surahNumber}`)}
+                    >
+                      `{surah.surahName}`
+                    </p>
+                  ))}
+                </div>
+              ) : null}
+            </TabsContent>
             <TabsContent value="bookmark">
               You do not have any bookmarks yet ?
             </TabsContent>
@@ -199,20 +217,133 @@ export default function IndexPage() {
               </TabsTrigger>
             </TabsList>
             <TabsContent value="surah">
+              {isLoading && (
+                <div className="">
+                  <div className="grid grid-cols-4 gap-2 items-center">
+                    <div role="status" className="max-w-sm animate-pulse">
+                      <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+                      <span className="sr-only">Loading...</span>
+                    </div>
+                    <div role="status" className="max-w-sm animate-pulse">
+                      <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+                      <span className="sr-only">Loading...</span>
+                    </div>
+                    <div role="status" className="max-w-sm animate-pulse">
+                      <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+                      <span className="sr-only">Loading...</span>
+                    </div>
+                    <div role="status" className="max-w-sm animate-pulse">
+                      <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+                      <span className="sr-only">Loading...</span>
+                    </div>
+                  </div>
+                  <div className="grid grid-cols-4 gap-2 items-center mt-5">
+                    <div role="status" className="max-w-sm animate-pulse">
+                      <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+                      <span className="sr-only">Loading...</span>
+                    </div>
+                    <div role="status" className="max-w-sm animate-pulse">
+                      <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+                      <span className="sr-only">Loading...</span>
+                    </div>
+                    <div role="status" className="max-w-sm animate-pulse">
+                      <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+                      <span className="sr-only">Loading...</span>
+                    </div>
+                    <div role="status" className="max-w-sm animate-pulse">
+                      <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+                      <span className="sr-only">Loading...</span>
+                    </div>
+                  </div>
+                </div>
+              )}
               <div className="grid sm:grid-cols-2 md:grid-cols-4 justify-between gap-10">
                 {surahs?.map((surah) => (
                   <div
-                    className="flex justify-between items-center border cursor-pointer border-gray-200 p-5 rounded-lg hover:border-primary group"
+                    className="grid grid-cols-5 gap-2 items-center border cursor-pointer border-gray-200 p-5 rounded-lg hover:border-primary group"
                     key={surah.id}
-                    onClick={() => navigate(`/recite/${surah.surahNumber}`)}
+                    onClick={() => {
+                      navigate(`/recite/${surah.surahNumber}`);
+                      // Age store kora data ber kore
+                      let storedSurahs =
+                        JSON.parse(localStorage.getItem("surahs")) || [];
+
+                      // Check kore dekhbo jeno duplicate na thake
+                      if (
+                        !storedSurahs.some(
+                          (s) => s.surahNumber === surah.surahNumber
+                        )
+                      ) {
+                        storedSurahs.push({
+                          surahNumber: surah.surahNumber,
+                          surahName: surah.surahName,
+                        });
+                      }
+
+                      // Update localStorage
+                      localStorage.setItem(
+                        "surahs",
+                        JSON.stringify(storedSurahs)
+                      );
+                    }}
                   >
-                    <div className="border rounded-sm bg-slate-400 group-hover:bg-primary text-white group-hover:text-wihite px-4 py-2  relative">
-                      <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-sm">{surah.surahNumber}</p>
+                    <div className="relative flex items-center justify-center w-[50px] h-[50px]">
+                      <span className="absolute text-black font-bold group-hover:text-primary">
+                        {surah.surahNumber}
+                      </span>
+                      <RubAlHizb
+                        width={50}
+                        height={50}
+                        className="group-hover:text-primary"
+                      />
                     </div>
-                    <div className="">
-                      <div className="flex gap-5">
-                        <p>{surah.surahName}</p>
-                        <p>
+
+                    <div className="col-span-4">
+                      <div className="flex gap-5 justify-between">
+                        <p className="font-bold text-md font-sans text-start">
+                          {surah.surahName}
+                        </p>
+                        <p className="font-arabic text-balck font-bold">
                           {
                             arabicSurahName.find(
                               (name) => name.id === surah.surahNumber
@@ -221,8 +352,10 @@ export default function IndexPage() {
                         </p>
                       </div>
 
-                      <div className="flex justify-end gap-5">
-                        <p>Ayah: {surah.totalAyah}</p>
+                      <div className="flex justify-between gap-5">
+                        <p className="font-bold text-sm text-primary-foreground group-hover:text-primary">
+                          Ayah: {surah.totalAyah}
+                        </p>
                       </div>
                     </div>
                   </div>
