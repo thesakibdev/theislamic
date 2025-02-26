f38af5f8fbe0e40a86c365f530efb3c762fe22e1
masaud04 <ahmedmasaud942@gmail.com>
2025-02-27 01:18:47 +0600

    donor page updated

--- client/src/pages/client/Donor.jsx
+++ client/src/pages/client/Donor.jsx
@@ -1,5 +1,14 @@
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 import { useGetAllDonorsQuery } from "../../slices/admin/donor";
 import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 export default function DonorPage() {
   const [currentPage, setCurrentPage] = useState(1);
@@ -11,35 +20,119 @@ export default function DonorPage() {
   return (
     <section className="pt-[4.5rem] md:pt-40">
       <div className="container mx-auto px-4">
-        <div className="">
+        <div className="max-w-5xl mx-auto">
           <h1 className="text-4xl font-semibold text-center">
             Honorable Donor List
           </h1>
-          <div className="">
-            {data?.donors?.map((donor) => (
+
+          {isLoading ? (
+            <p>Loading...</p>
+          ) : data?.donors?.length > 0 ? (
+            data?.donors?.map((donor) => (
               <div
                 key={donor.name}
-                className="border p-4 grid md:grid-cols-4 items-center justify-between my-6 gap-2"
+                className="border p-4 grid grid-cols-1 md:grid-cols-5 my-6 gap-5 rounded-md relative"
               >
-                {donor.avatar === "" ? (
-                  <div className="w-[100px] h-[100px] mx-auto bg-gray-300"></div>
-                ) : (
-                  <img src={donor.avatar} alt={donor.name} />
-                )}
-                <div className="md:col-span-3">
-                  <h2>Name: {donor.name}</h2>
-                  <div className="grid md:grid-cols-3">
-                    <p>Profession: {donor.profession}</p>
-                    <p>Company: {donor.companyName}</p>
-                    <p>Designation: {donor.designation}</p>
-                    <p>Street: {donor.street}</p>
-                    <p>City: {donor.city}</p>
-                    <p>Country: {donor.country}</p>
+                <div className="md:col-span-1 overflow-hidden rounded-md">
+                  {donor.avatar === "" ? (
+                    <div className="w-full h-96 md:h-[200px] mx-auto bg-gray-300"></div>
+                  ) : (
+                    <div className="max-h-[400px] md:max-h-[200px] w-full object-fill">
+                      <img
+                        src={donor.avatar}
+                        alt={donor.name}
+                        className="rounded-md h-full w-full object-cover mx-auto md:mx-0"
+                      />
+                    </div>
+                  )}
+                </div>
+                <div className="md:col-span-4">
+                  <p className="text-xl md:text-2xl font-bold mb-2">
+                    Name: {donor.name}
+                  </p>
+                  <div className="grid md:grid-cols-2 gap-y-3 gap-x-10">
+                    <p className="text-lg text-black/50">
+                      <span className="font-semibold text-black/60">
+                        Profession:
+                      </span>{" "}
+                      {donor.profession}
+                    </p>
+                    <p className="text-lg text-black/50">
+                      <span className="font-semibold text-black/60">
+                        Company:
+                      </span>{" "}
+                      {donor.companyName}
+                    </p>
+                    <p className="text-lg text-black/50">
+                      <span className="font-semibold text-black/60">
+                        Designation:{" "}
+                      </span>
+                      {donor.designation}
+                    </p>
+                    <p className="text-lg text-black/50">
+                      <span className="font-semibold text-black/60">
+                        Street:
+                      </span>{" "}
+                      {donor.street}
+                    </p>
+                    <p className="text-lg text-black/50">
+                      <span className="font-semibold text-black/60">City:</span>{" "}
+                      {donor.city}
+                    </p>
+                    <p className="text-lg text-black/50">
+                      <span className="font-semibold text-black/60">
+                        Country:
+                      </span>{" "}
+                      {donor.country}
+                    </p>
                   </div>
                 </div>
               </div>
-            ))}
-          </div>
+            ))
+          ) : (
+            <p>No Donors Found</p>
+          )}
+        </div>
+        <div className="flex justify-center">
+          <Pagination className="px-4">
+            <PaginationContent>
+              <PaginationItem>
+                <PaginationPrevious
+                  aria-label="Go to previous page"
+                  onClick={() =>
+                    setCurrentPage(() =>
+                      currentPage == 1 ? 1 : currentPage - 1
+                    )
+                  }
+                  className="bg-primary hover:bg-green-400 cursor-pointer text-white"
+                  disabled={currentPage == 1}
+                />
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationEllipsis />
+              </PaginationItem>
+              <PaginationItem>
+                <Button
+                  className="bg-primary hover:bg-green-400 cursor-pointer text-white"
+                  onClick={() => setCurrentPage(() => 
+                    currentPage > 2 ? 1 : currentPage + 2
+                  )}
+                >
+                  Skip
+                </Button>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationEllipsis />
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationNext
+                  aria-label="Go to next page"
+                  onClick={() => setCurrentPage(currentPage + 1)}
+                  className="bg-primary hover:bg-green-400 cursor-pointer text-white"
+                />
+              </PaginationItem>
+            </PaginationContent>
+          </Pagination>
         </div>
       </div>
     </section>
