0a9795825498ddf44cf4658c3d07c339015b60c5
The Sakib Dev <139985754+thesakibdev@users.noreply.github.com>
2025-04-09 22:44:03 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const tafsirRouter = require("./routes/tafsir/tafsir.route");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 const allowedOrigins = [
   "http://localhost:5173",
