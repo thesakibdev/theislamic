6e5286073c2d61c2b4b4ae14a531c10f93c2c6b8
The Sakib Dev <139985754+thesakibdev@users.noreply.github.com>
2025-04-10 08:50:58 +0600

    Create index.js

---
+++ frontend/src/slices/admin/tafsir/index.js
@@ -0,0 +1,63 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const baseUrl = import.meta.env.VITE_BASE_URL;
+
+export const tafsirApi = createApi({
+  reducerPath: "tafsirApi",
+  baseQuery: fetchBaseQuery({ baseUrl, credentials: "include" }),
+  tagTypes: ["tafsir"],
+  endpoints: (builder) => ({
+    // Add a new tafsir
+    addTafsir: builder.mutation({
+      query: (formData) => ({
+        url: "/admin/tafsir/add",
+        method: "POST",
+        body: formData,
+        headers: { "Content-Type": "application/json" },
+      }),
+      invalidatesTags: ["tafsir"],
+    }),
+
+    // Edit an existing tafsir
+    editTafsir: builder.mutation({
+      query: ({ id, language, ...formData }) => ({
+        url: `/admin/tafsir/edit/${language}/${id}`,
+        method: "PUT",
+        body: formData,
+        headers: { "Content-Type": "application/json" },
+      }),
+      invalidatesTags: ["tafsir"],
+    }),
+
+    // Delete a specific tafsir
+    deleteTafsir: builder.mutation({
+      query: ({ id, language }) => ({
+        url: `/admin/tafsir/edit/${language}/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Surah", "Verse"],
+    }),
+
+    // Fetch paginated Tafsir data
+    getAllTafsirPaginated: builder.query({
+      query: ({
+        language,
+        bookName,
+        totalVerseNumber,
+        page = 1,
+        limit = 10,
+      }) => ({
+        url: `/admin/tafsir/get?language=${language}&bookName=${bookName}&totalVerseNumber=${totalVerseNumber}&page=${page}&limit=${limit}`,
+        method: "GET",
+      }),
+      providesTags: ["tafsir"],
+    }),
+  }),
+});
+
+export const {
+  useAddTafsirMutation,
+  useEditTafsirMutation,
+  useDeleteTafsirMutation,
+  useGetAllTafsirPaginatedQuery,
+} = tafsirApi;
