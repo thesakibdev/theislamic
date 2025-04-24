0cb498880c68c6fb8e0bd9070f8094a84a87edca
The Sakib Dev <139985754+thesakibdev@users.noreply.github.com>
2025-04-25 03:02:27 +0600

    Update tafsir.route.js

--- backend/routes/tafsir/tafsir.route.js
+++ backend/routes/tafsir/tafsir.route.js
@@ -4,6 +4,7 @@ const {
   getTafsir,
   editTafsir,
   deleteTafsir,
+  getAllTafsirByTotalVerseNumber,
 } = require("../../controllers/tafsir/tafsir.controller");
 
 const router = express.Router();
@@ -12,5 +13,8 @@ router.post("/add", addTafsir);
 router.get("/get", getTafsir);
 router.put("/edit/:language/:id/:bookName", editTafsir);
 router.delete("/delete/:language/:id/:bookName", deleteTafsir);
+
+// client side router
+router.get("/get/single",getAllTafsirByTotalVerseNumber)
 
 module.exports = router;
