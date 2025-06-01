01ba4003cbff58bfcb2e5888cd566f4479e10f6f
The Sakib Dev <139985754+thesakibdev@users.noreply.github.com>
2025-06-01 10:18:19 +0600

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -35,7 +35,7 @@ import { useEffect } from "react";
 import { checkAuth } from "./slices/authslice";
 // import { Skeleton } from "./components/ui/skeleton";
 import SearchResult from "./pages/client/SearchResult";
-// import TafsirPage from "./pages/client/tafsir/TafsirPage";
+import TafsirIndexPage from "./pages/client/tafsir/TafsirIndex";
 
 export default function App() {
   const { user, isAuthenticated, isLoading } = useSelector(
@@ -64,7 +64,7 @@ export default function App() {
         <Route path="/index" element={<IndexPage />} />
         <Route path="/translation" element={<Translation />} />
         <Route path="/profile" element={<AuthProfile />} />
-        {/* <Route path="/tafsir" element={<TafsirPage />} /> */}
+        <Route path="/tafsir" element={<TafsirIndexPage />} />
         <Route path="/" element={<ReciteLayout />}>
           <Route path="/recite/:number" element={<RecitePage />} />
         </Route>
